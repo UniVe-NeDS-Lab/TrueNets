ed4f06b046b71d95ae1f8f1ec1a259ae3d31aba3
Gabriele Gemmi <gabriele.gemmi@studenti.unitn.it>
2022-03-30 18:20:10 +0000

    point selection comparison

--- analysis/points_selection_comparison.py
+++ analysis/points_selection_comparison.py
@@ -6,16 +6,20 @@ def perc_diff(a, b):
     return (a-b)/a
 
 
+def diff(a, b):
+    return (a-b)
+
+
 print("area centroids high best_hp")
 for d in datasets:
-    best_p = int(subprocess.getoutput(f"wc -l results/{d}/best_p_intervisibility.edgelist | cut -d' ' -f1"))
-    centroids = int(subprocess.getoutput(f"wc -l results/{d}/centroids_intervisibility.edgelist | cut -d' ' -f1"))
-    high_p = int(subprocess.getoutput(f"wc -l results/{d}/high_p_intervisibility.edgelist | cut -d' ' -f1"))
-    best_p_hp = int(subprocess.getoutput(f"wc -l results/{d}/best_p_hp_intervisibility.edgelist | cut -d' ' -f1"))
+    best_p = int(subprocess.getoutput(f"wc -l ../results/{d}/best_p_intervisibility.edgelist | cut -d' ' -f1"))
+    centroids = int(subprocess.getoutput(f"wc -l ../results/{d}/centroids_intervisibility.edgelist | cut -d' ' -f1"))
+    high_p = int(subprocess.getoutput(f"wc -l ../results/{d}/high_p_intervisibility.edgelist | cut -d' ' -f1"))
+    best_p_hp = int(subprocess.getoutput(f"wc -l ../results/{d}/best_p_hp_intervisibility.edgelist | cut -d' ' -f1"))
 
-    print(f"{d} {perc_diff(best_p, centroids):.3f} {perc_diff(best_p, high_p):.3f} {perc_diff(best_p, best_p_hp):.3f}")
+    print(f"{d} {perc_diff(best_p, centroids):.3f} {perc_diff(best_p, high_p):.3f} {perc_diff(best_p, best_p_hp):.3f}  {diff(best_p, centroids)} {diff(best_p, high_p)} {diff(best_p, best_p_hp)}")
 
-
+exit(0)
 # Cartesian distance between points
 distances = []
 for idx, d in enumerate(datasets):
